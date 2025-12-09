c309169fc23c7eb16dbc9cd2b6ed68afd5ce6579
SanghyeonYeah <jeff090413@gmail.com>
2025-12-09 19:38:45 +0900

    ehlsemt

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
+const passport = require('passport');
 const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 require('dotenv').config();
@@ -48,6 +49,9 @@ app.use(session({
     sameSite: 'lax'
   }
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 /* ===== CSRF ===== */
 const csrfProtection = csrf({ cookie: true });
